a966398f2ae81ccbc0603c438da92906aad8ff32
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-01-22 14:00:27 +0300

    Create travel_blog.dart

---
+++ lib/widgets/travel_blog.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:travel_blog/model/travel.dart';
+
+class TravelBlog extends StatelessWidget {
+  final _list=Travel.generateTravelBlog();
+  final _pageCtrl=PageController(viewportFraction: 0.9);
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView.builder(itemBuilder: (context,index){
+      var travel= _list[index];
+          return Stack(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(5),
+                child: Image.asset(travel.url),
+              )
+            ],
+          );
+    });
+  }
+}
