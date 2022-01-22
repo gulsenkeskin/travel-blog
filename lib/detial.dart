8767a2444bed345834e16c02149276849c0d20fd
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-01-22 15:35:17 +0300

    Update detial.dart

--- lib/detial.dart
+++ lib/detial.dart
@@ -1,17 +1,68 @@
-
 import 'package:flutter/material.dart';
 
 import 'model/travel.dart';
 
 class DetailPage extends StatelessWidget {
   final Travel travel;
+  final double expandedHeight = 300;
 
   DetailPage({Key? key, required this.travel}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: Stack(
+        children: [
+          CustomScrollView(
+            slivers: [_buildSilverHead()],
+          )
+        ],
+      ),
     );
+  }
+
+  Widget _buildSilverHead() {
+    return SliverPersistentHeader(
+        delegate: DetailSliverDelegate(
+            travel: travel, expandedHeight: expandedHeight));
+  }
+}
+
+class DetailSliverDelegate extends SliverPersistentHeaderDelegate {
+  final Travel travel;
+  final double expandedHeight;
+
+  DetailSliverDelegate({required this.travel, required this.expandedHeight});
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Stack(
+      children: [
+        Opacity(
+          opacity: 0.8,
+          child: Image.asset(
+            travel.url,
+            width: MediaQuery.of(context).size.width,
+            height: expandedHeight,
+            fit: BoxFit.cover,
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  // TODO: implement maxExtent
+  double get maxExtent => expandedHeight;
+
+  @override
+  // TODO: implement minExtent
+  double get minExtent => 0;
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
+    // TODO: implement shouldRebuild
+    return true;
   }
 }
