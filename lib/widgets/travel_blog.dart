e512f5a04646d965a0ec653164282eb43ce07d10
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-01-22 14:03:16 +0300

    Update travel_blog.dart
    img width double.infinity

--- lib/widgets/travel_blog.dart
+++ lib/widgets/travel_blog.dart
@@ -2,21 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:travel_blog/model/travel.dart';
 
 class TravelBlog extends StatelessWidget {
-  final _list=Travel.generateTravelBlog();
-  final _pageCtrl=PageController(viewportFraction: 0.9);
+  final _list = Travel.generateTravelBlog();
+  final _pageCtrl = PageController(viewportFraction: 0.9);
 
   @override
   Widget build(BuildContext context) {
-    return PageView.builder(itemBuilder: (context,index){
-      var travel= _list[index];
-          return Stack(
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(5),
-                child: Image.asset(travel.url),
-              )
-            ],
-          );
+    return PageView.builder(itemBuilder: (context, index) {
+      var travel = _list[index];
+      return Stack(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(5),
+            child: Image.asset(
+              travel.url,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+          )
+        ],
+      );
     });
   }
 }
