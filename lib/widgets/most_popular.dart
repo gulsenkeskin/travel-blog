5eba51607e93ef53f30d9bf198e492a861dbb48a
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-01-22 15:10:52 +0300

    Update most_popular.dart

--- lib/widgets/most_popular.dart
+++ lib/widgets/most_popular.dart
@@ -20,6 +20,32 @@ class MostPopular extends StatelessWidget {
                 width: 140,
                 fit: BoxFit.cover,
               ),
+            ),
+            Positioned(
+              bottom: 20,
+              left: 15,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Material(
+                    color: Colors.transparent,
+                    child: Text(
+                      travel.location,
+                      style: TextStyle(color: Colors.white, fontSize: 15),
+                    ),
+                  ),
+                  Material(
+                    color: Colors.transparent,
+                    child: Text(
+                      travel.name,
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20),
+                    ),
+                  )
+                ],
+              ),
             )
           ],
         );
