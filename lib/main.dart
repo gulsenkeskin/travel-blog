d4dd9eb5c1456aef284a173539d5f20b060c888e
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-01-22 12:48:38 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:travel_blog/home.dart';
 
 void main(){
   runApp(MyApp());
@@ -9,9 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: Scaffold(
-        body: Container(),
+        body: HomePage(),
       ),
     );
   }
