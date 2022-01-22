553cb8997412419c1b5af0253712380361d2ba65
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-01-22 14:06:46 +0300

    Update travel_blog.dart

--- lib/widgets/travel_blog.dart
+++ lib/widgets/travel_blog.dart
@@ -7,23 +7,25 @@ class TravelBlog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return PageView.builder(itemBuilder: (context, index) {
-      var travel = _list[index];
-      return Stack(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(right: 20, bottom: 30),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(5),
-              child: Image.asset(
-                travel.url,
-                width: MediaQuery.of(context).size.width,
-                fit: BoxFit.cover,
-              ),
-            ),
-          )
-        ],
-      );
-    });
+    return PageView.builder(
+        controller: _pageCtrl,
+        itemBuilder: (context, index) {
+          var travel = _list[index];
+          return Stack(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(right: 20, bottom: 30),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(5),
+                  child: Image.asset(
+                    travel.url,
+                    width: MediaQuery.of(context).size.width,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              )
+            ],
+          );
+        });
   }
 }
