93960b2d440ce29df937d62b0f70c22d5c01255e
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-01-22 13:44:10 +0300

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -9,11 +9,43 @@ class HomePage extends StatelessWidget {
       backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.white,
-        elevation: 0,// appbarın altındaki çizgiyi siler
+        elevation: 0, // appbarın altındaki çizgiyi siler
         actions: [
           Padding(
             padding: const EdgeInsets.only(right: 10),
-            child: Icon(Icons.menu,color: Colors.black,),
+            child: Icon(
+              Icons.menu,
+              color: Colors.black,
+            ),
+          )
+        ],
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 15),
+            child: Text(
+              'Travel Blog',
+              style: TextStyle(color: Colors.black, fontSize: 36),
+            ),
+          ),
+          Expanded(flex: 2, child: Placeholder()),
+          Padding(
+            padding: const EdgeInsets.all(15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Most Popular',
+                  style: TextStyle(fontSize: 20),
+                ),
+                Text(
+                  'View All',
+                  style: TextStyle(fontSize: 16, color: Colors.deepOrange),
+                )
+              ],
+            ),
           )
         ],
       ),
