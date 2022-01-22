8f133ef4c0a3a5c89c00864707b59b493326bbed
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-01-22 12:53:28 +0300

    Update home.dart
    added menu icon to appbar

--- lib/home.dart
+++ lib/home.dart
@@ -5,6 +5,17 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,// appbarın altındaki çizgiyi siler
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 10),
+            child: Icon(Icons.menu,color: Colors.black,),
+          )
+        ],
+      ),
+    );
   }
 }
